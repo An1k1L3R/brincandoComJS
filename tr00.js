105cfa1f96fee5f0ded9a4870de124f322826186
vinni.code <60686497+An1k1L3R@users.noreply.github.com>
2021-10-01 02:14:29 -0300

    add via upload
    não é o que vc quer fazer o ideal é criar um repositório remoto e enviar os arquivos do terminal git-bash

---
+++ tr00.js
@@ -0,0 +1,9 @@
+var n1 = window.prompt("Digite um número");
+if(n1 <= 5){
+    valor1 = n1;
+    window.alert("o valor é " + valor1 + " é menor que 5");
+}
+else{
+    valor2 = n1;
+    window.alert("o valor é " + valor2 + " é maior que 5");
+}
